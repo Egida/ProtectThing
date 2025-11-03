04769d5c6c8cadd1310e19083fe42940366b325f
Thoq <tristanshaw000@gmail.com>
2025-11-03 17:40:37 -0500

    something

---
+++ out/CMakeFiles/ProtectThing.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProtectThing.dir/src/config/impl/config_parser.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/config/impl/config_parser.cpp.o.d"
+  "CMakeFiles/ProtectThing.dir/src/features/impl/ddos_protection.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/features/impl/ddos_protection.cpp.o.d"
+  "CMakeFiles/ProtectThing.dir/src/main.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/main.cpp.o.d"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/arg_parse.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/arg_parse.cpp.o.d"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/fs.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/fs.cpp.o.d"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/logger.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/logger.cpp.o.d"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/misc.cpp.o"
+  "CMakeFiles/ProtectThing.dir/src/util/impl/misc.cpp.o.d"
+  "ProtectThing"
+  "ProtectThing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProtectThing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
